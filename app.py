f3e5f8a41ced2618de13c4de32b547370bd32dfb
Nkosinam Dlungwana <139022461+Awakenned1@users.noreply.github.com>
2024-11-10 16:37:40 +0200

    updated structure

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
-from routes import bp as main_bp
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.register_blueprint(main_bp)
+
+@app.route('/')
+def dashboard():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
